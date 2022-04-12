cd7ff0b583d32192779ef1c34b3decb4cb232f01
Samiya <skmcintosh20@gmail.com>
2022-04-11 17:08:30 -0700

    Finished Header Component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 function App() {
 	return (
 		<div className="app">
-			<h1>Date Me</h1>
 			{/* Header */}
 			<Header />
 			{/* Date Cards */}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "../styles/Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import { IconButton } from "@material-ui/core";
+import Logo from "../assets/TL.png";
+import ForumIcon from "@material-ui/icons/Forum";
 
 function Header() {
-	return <div className="header">Header</div>;
+	return (
+		<div className="header">
+			<IconButton>
+				<PersonIcon fontSize="large" className="header__icon" />
+			</IconButton>
+
+			<img className="header__logo" src={Logo} alt="" />
+			<IconButton>
+				<ForumIcon fontSize="large" className="header__icon" />
+			</IconButton>
+		</div>
+	);
 }
 
 export default Header;
